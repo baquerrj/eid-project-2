0929fe606f194c84edafaf8af1c43123f5ce0b2e
Zach Turner <zachturner017@gmail.com>
2021-01-30 14:45:20 -0600

    sensors varying data, beginning to incorproate mysql into node

--- sensor.js
+++ sensor.js
@@ -9,6 +9,27 @@ process.on('message', (msg) => {
 
 let counter = 0;
 
+let alarm_count = 0
+let error_count = 0
+
+// These events happen once every ten seconds since interval is in milliseconds
 setInterval(() => {
-process.send({ counter: counter++, id_num: id, temp: temp_threshold });
+    // Theses values should be within [-2, 2)
+    let temp_offset = (Math.random() * 4) - 2
+    let temp_measurement = temp_threshold + temp_offset
+    
+    // should be within 0-9
+    let random_event = Math.floor(Math.random() * 10)
+
+    if (random_event == 0)
+    {
+        // temperature spike occured
+    }
+    else if(random_event == 9)
+    {
+        // no temperature available
+        temp_measurement = 999
+    }
+
+    process.send({ counter: counter++, id_num: id, temp: temp_measurement });
 }, 1000);
--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const args = process.argv.slice(2)
 // the number of sensors is the first argument
 const num_sensors = args[0]
 
+// Initialize array for sensors
 let sensors = [];
 sensors.length = num_sensors
 
 console.log('number of sensors', num_sensors)
 
+// temperature to vary thresholds around
 const temp_threshold = 40.0
 
 for (let index = 0; index < num_sensors; index++) {
@@ -18,6 +20,16 @@ for (let index = 0; index < num_sensors; index++) {
     
     let sensor_temp = temp_threshold
 
+    // value of offset should be 0 or 1 since random returns
+    // between [0,1)
+    let sensor_offset = Math.floor(Math.random() * 2)
+
+    if (sensor_offset > 0)
+        sensor_temp += 5.0
+    else
+        sensor_temp -= 5.0
+
+    // pass configuration to the forked sensor process to initialize
     sensors[index].send({number: index, temperature: sensor_temp})
 
     sensors[index].on('message', (msg) => {
