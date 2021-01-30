c49b342f4e32876049c396c52993663fdc8f7ade
Zach Turner <zachturner017@gmail.com>
2021-01-30 13:13:14 -0600

    passing configuration to sensors

--- sensor.js
+++ sensor.js
@@ -1,11 +1,14 @@
+let id = 0
+let temp_threshold = 0
+
 process.on('message', (msg) => {
-    console.log('Message from parent:', msg);
+    console.log('Message from parent:', msg)
+    id = msg["number"]
+    temp_threshold = msg["temperature"]
   });
 
-  
-  
-  let counter = 0;
-  
-  setInterval(() => {
-    process.send({ counter: counter++ });
-  }, 1000);
+let counter = 0;
+
+setInterval(() => {
+process.send({ counter: counter++, id_num: id, temp: temp_threshold });
+}, 1000);
--- server.js
+++ server.js
@@ -7,17 +7,21 @@ const args = process.argv.slice(2)
 const num_sensors = args[0]
 
 let sensors = [];
-sensors.length = num_sensors;
+sensors.length = num_sensors
 
-console.log('number of sensors', num_sensors);
+console.log('number of sensors', num_sensors)
+
+const temp_threshold = 40.0
 
 for (let index = 0; index < num_sensors; index++) {
     sensors[index] = fork('sensor.js')
     
-    sensors[index].send({number: index});
+    let sensor_temp = temp_threshold
+
+    sensors[index].send({number: index, temperature: sensor_temp})
 
     sensors[index].on('message', (msg) => {
-        console.log('Message from child ' + index, msg);
+        console.log('Message from child ' + index, msg)
     });
 }
 
