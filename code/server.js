f4c00552f69e1c2ae5762feb230213f46688116b
Roberto Baquerizo <roba8460@colorado.edu>
2021-01-31 12:09:29 -0700

    updated to create database for sensor data and table in db

--- code/server.js
+++ code/server.js
@@ -1,5 +1,49 @@
 const { fork } = require('child_process')
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "sensor",
+  password: "balderdash"
+});
+
+con.connect(function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("Connected!");
+
+    var sql = "CREATE DATABASE IF NOT EXISTS sensor_db";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Database created!");
+    });
+
+    con.changeUser({
+        database: 'sensor_db'
+    }, (err) => {
+        if (err) {
+            console.log('Error in changing database ', err);
+            return;
+        }
+    });
+
+    var sql = "DROP TABLE IF EXISTS sensors";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table deleted");
+    });
+
+    var sql = "CREATE TABLE sensors (id INT AUTO_INCREMENT PRIMARY KEY, sensorId INTEGER(255), temperature DOUBLE(5,3))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+
+  });
+
 // get the arguments after "node server.js ..."
 const args = process.argv.slice(2)
 
@@ -17,7 +61,7 @@ const temp_threshold = 40.0
 
 for (let index = 0; index < num_sensors; index++) {
     sensors[index] = fork('sensor.js')
-    
+
     let sensor_temp = temp_threshold
 
     // value of offset should be 0 or 1 since random returns
