7684054619551f1e639f12f3cbd54ef94011c634
Roberto Baquerizo <roba8460@colorado.edu>
2021-01-31 11:58:29 -0700

    added SQL query to add sensor id and temperature to table
    commented out connection.end() stuff until we can figure out how to close all connections when program exits

--- code/sensor.js
+++ code/sensor.js
@@ -6,15 +6,18 @@ process.on('message', (msg) => {
     temp_threshold = msg["temperature"]
   });
 
-const db_connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'sensor',
-    password: 'balderdash'
-})
+var config = {
+   user: 'sensor',
+   host: 'localhost',
+   password: 'balderdash',
+   database: 'sensor_db'
+};
+
+const db_connection = mysql.createConnection(config);
 
 db_connection.connect((err) => {
     if (err) {
-        console.log('Error connecting to database')
+        console.log('Error connecting to database: ' + err );
         return
     }
     console.log('Sensor-' + id + ' connected to database')
@@ -33,7 +36,7 @@ setInterval(() => {
     // Theses values should be within [-2, 2)
     let temp_offset = (Math.random() * 4) - 2
     let temp_measurement = temp_threshold + temp_offset
-    
+
     // should be within 0-9
     let random_event = Math.floor(Math.random() * 10)
 
@@ -47,12 +50,21 @@ setInterval(() => {
         temp_measurement = 999
     }
 
+    var sql = "INSERT INTO sensors (sensorId, temperature) VALUES ?";
+    var values = [
+        [id, temp_measurement]
+    ];
+    db_connection.query(sql, [values], function (err, result) {
+        if (err) console.log(err);
+        console.log("Number of records insert: " + result.affectedRows);
+    })
     process.send({ counter: counter++, id_num: id, temp: temp_measurement });
 }, 1000);
 
-db_connection.end((err) => {
-    if (err) {
-        console.log('Error closing connection to database')
-        return
-    }
-  });
+// db_connection.end((err) => {
+//     if (err) {
+//         console.log('Error closing connection to database')
+//         return
+//     }
+//     console.log('Connection closed!');
+//   });
